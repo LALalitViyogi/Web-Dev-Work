06375d07c3ece2de552f3dbce8348ba3c6aab741
L.A. Lalit Viyogi <viyogi.lalit786@gmail.com>
2021-07-06 00:30:05 +0530

    product page Finalized

--- Viyogi_Enterprise/main.py
+++ Viyogi_Enterprise/main.py
@@ -5,7 +5,8 @@ import json
 
 app = Flask(__name__)
 
-img=["viyogi_notes.png","adi_image_logo.png","viyogi_notes.png","adi_image_logo.png","viyogi_notes.png","adi_image_logo.png","viyogi_notes.png","adi_image_logo.png"]
+img=["viyogi_notes.png","adi_image_logo.png","covid_logo.png","express_calcy_logo.png",
+     "aditya_secura_logo.png","vidat_logo.png","chekaut_logo.png","adi_image_logo.png"]
 new_product=[2,3,5,7,6,1] ## indices of all product that should be on main page
 @app.route('/')
 def main():
@@ -26,7 +27,7 @@ def signup():
 @app.route('/product')
 def products():
     
-    product=["Viyogi Notes","Adi Images","Covid Data","Express-Calcy","Aditya Centre","V-Data Tool","CheKaut","eighth"]
+    product=["Viyogi Notes","Adi Images","Covid Data","Express-Calcy","Aditya Secura","ViDaT Tool","CheKaut","eighth"]
     link=["http://www.google.com/","http://www.github.com/","#","#","#","#","#","#"]
     color=["255,138,101","255,202,40","102,187,106","41,182,246","240,98,146","60,150,85","159,168,218","197,225,165"]
     return render_template('product.html',products=product,links=link,colors=color,imgs=img ,len=len(img))
