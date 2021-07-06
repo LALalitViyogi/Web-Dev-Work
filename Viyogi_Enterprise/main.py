1a9d85e9b0e72f7dcacc39507c5bf3d3e4a7255a
L.A. Lalit Viyogi <viyogi.lalit786@gmail.com>
2021-07-07 00:16:04 +0530

    home page backend updated

--- Viyogi_Enterprise/main.py
+++ Viyogi_Enterprise/main.py
@@ -6,11 +6,25 @@ import json
 app = Flask(__name__)
 
 img=["viyogi_notes.png","adi_image_logo.png","covid_logo.png","express_calcy_logo.png",
-     "aditya_secura_logo.png","vidat_logo.png","chekaut_logo.png","adi_image_logo.png"]
-new_product=[2,3,5,7,6,1] ## indices of all product that should be on main page
+     "aditya_secura_logo.png","vidat_logo.png","chekaut_logo.png","inserminal_logo.png"]
+
+main_page_product=[2,3,5,7,6,1]
+
+def update(products):  ## indices of all product that should be on main page
+    global img
+    new_product=[2,3,5,7,6,1]
+    
+    if type(products)== type(list()) and products is not None:
+        indices=list()
+        for product in products:
+            indices.append(img.index(product))
+        return indices
+    else:
+        return new_product
+    
 @app.route('/')
 def main():
-    return render_template('home.html',imgs=img,len=new_product)
+    return render_template('home.html',imgs=img,len=main_page_product)
 
 @app.route('/contact')
 def cont():
@@ -27,7 +41,7 @@ def signup():
 @app.route('/product')
 def products():
     
-    product=["Viyogi Notes","Adi Images","Covid Data","Express-Calcy","Aditya Secura","ViDaT Tool","CheKaut","eighth"]
+    product=["Viyogi Notes","Adi Images","Covid Data","Express-Calcy","Aditya Secura","ViDaT Tool","CheKaut","Inserminal"]
     link=["http://www.google.com/","http://www.github.com/","#","#","#","#","#","#"]
     color=["255,138,101","255,202,40","102,187,106","41,182,246","240,98,146","60,150,85","159,168,218","197,225,165"]
     return render_template('product.html',products=product,links=link,colors=color,imgs=img ,len=len(img))
