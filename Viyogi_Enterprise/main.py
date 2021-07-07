1b61fa375fd32c8087435354fdfae36ab1d8a96d
L.A. Lalit Viyogi <viyogi.lalit786@gmail.com>
2021-07-07 23:40:01 +0530

    main page done and admin page added

--- Viyogi_Enterprise/main.py
+++ Viyogi_Enterprise/main.py
@@ -6,14 +6,21 @@ import json
 app = Flask(__name__)
 
 img=["viyogi_notes.png","adi_image_logo.png","covid_logo.png","express_calcy_logo.png",
-     "aditya_secura_logo.png","vidat_logo.png","chekaut_logo.png","inserminal_logo.png"]
+"aditya_secura_logo.png","vidat_logo.png","chekaut_logo.png","inserminal_logo.png"]
+
+product=["Viyogi Notes","Adi Images","Covid Data","Express-Calcy","Aditya Secura",
+"ViDaT Tool","CheKaut","Inserminal"]
+
+link=["http://www.google.com/","http://www.github.com/","#","https://facebook.com",
+"viyogilearnings.blogspot.com","#","#","#"]
+color=["255,138,101","255,202,40","102,187,106","41,182,246",
+"240,98,146","60,150,85","159,168,218","197,225,165"]
 
 main_page_product=[2,3,5,7,6,1]
 
 def update(products):  ## indices of all product that should be on main page
     global img
     new_product=[2,3,5,7,6,1]
-    
     if type(products)== type(list()) and products is not None:
         indices=list()
         for product in products:
@@ -21,10 +28,16 @@ def update(products):  ## indices of all product that should be on main page
         return indices
     else:
         return new_product
-    
+
+def Login():
+    pass
 @app.route('/')
 def main():
-    return render_template('home.html',imgs=img,len=main_page_product)
+    return render_template('home.html',imgs=img,len=main_page_product,links=link)
+
+@app.route('/adlog')
+def logpage():
+    return render_template('login.html')
 
 @app.route('/contact')
 def cont():
@@ -40,10 +53,6 @@ def signup():
 
 @app.route('/product')
 def products():
-    
-    product=["Viyogi Notes","Adi Images","Covid Data","Express-Calcy","Aditya Secura","ViDaT Tool","CheKaut","Inserminal"]
-    link=["http://www.google.com/","http://www.github.com/","#","#","#","#","#","#"]
-    color=["255,138,101","255,202,40","102,187,106","41,182,246","240,98,146","60,150,85","159,168,218","197,225,165"]
     return render_template('product.html',products=product,links=link,colors=color,imgs=img ,len=len(img))
 
 @app.route('/project')
